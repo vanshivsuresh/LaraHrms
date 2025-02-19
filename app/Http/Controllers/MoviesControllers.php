1ea9d1ec32996f914f05302689bd77f48d4feab7
suresh <suresh.yadav@vanshiv.com>
2025-02-19 14:16:14 +0530

    changes under store

--- app/Http/Controllers/MoviesControllers.php
+++ app/Http/Controllers/MoviesControllers.php
@@ -24,7 +24,7 @@ class MoviesControllers extends Controller
     }
 
     /**
-     * Store movies detailed data. 
+     * Store movies detailed data added the poster on filepath. 
      */
     public function store(Request $request)
     {
