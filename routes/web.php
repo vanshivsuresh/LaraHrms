4330f556651b0e77792d87c85b3a3978b69539c9
suresh <suresh.yadav@vanshiv.com>
2025-02-20 20:45:49 +0530

    pushed changes

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+//addin the comments for the testing..
 Route::resource('/movies',MoviesControllers::class);
 // Route::get('/movies/create', [MovieController::class, 'create'])->name('movies.create');
 // Route::post('/movies', [MovieController::class, 'store'])->name('movies.store');
